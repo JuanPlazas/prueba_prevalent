caedf7734f54bc393fb6ce0efe2e51f4f90c524e
juanplazas <juan-2151@hotmail.com>
2024-07-03 12:28:10 -0500

    se crean los queries y mutations para el manejo de datos en graphql

--- pages/api/graphql.ts
+++ pages/api/graphql.ts
@@ -3,24 +3,31 @@ import { startServerAndCreateNextHandler } from '@as-integrations/next';
 import { makeExecutableSchema } from '@graphql-tools/schema';
 import { gql } from 'graphql-tag';
 
+/** importacion de nuestros typeDefs */
+import {IngresosEgresosConceptosTypeDefs} from "./graphql/types/ingresos_egresos_conceptos"
+import {UserRolesTypeDefs} from "./graphql/types/user_rol"
+import {UsersTypeDefs} from "./graphql/types/user"
+import {IngresosEgresosTypeDefs} from "./graphql/types/ingresos_egresos"
+
+/** importacion de nuestros resolvers */
+import IngresosEgresosConceptosResolvers from "./graphql/resolvers/ingresos_egresos_conceptos"
+import UserRolesResolvers from "./graphql/resolvers/user_rol"
+import UsersResolvers from "./graphql/resolvers/user"
+import IngresosEgresosResolvers from "./graphql/resolvers/ingresos_egresos"
+
 const typeDefs = gql`
-  type Query {
-    users: [User!]!
-  }
-
-  type User {
-    name: String
-    username: String
-  }
+  ${IngresosEgresosConceptosTypeDefs}
+  ${UserRolesTypeDefs}
+  ${UsersTypeDefs}
+  ${IngresosEgresosTypeDefs}
 `;
-
-const users = [{ name: 'Foo Bar', username: 'foobar' }];
 
 const resolvers = {
   Query: {
-    users() {
-      return users;
-    },
+    ...IngresosEgresosConceptosResolvers,
+    ...UserRolesResolvers,
+    ...UsersResolvers,
+    ...IngresosEgresosResolvers,
   },
 };
 
---
+++ pages/api/graphql/types/user.ts
@@ -0,0 +1,44 @@
+export const UsersTypeDefs = `
+  type Query {
+    getUsers: [User]!
+  }
+
+  type Query {
+    createUser(input: UserCreateInput!): User
+  }
+
+  type Query {
+    getUser(input: UserIdInput!): User
+  }
+
+  type Query {
+    updateUser(input: UserCreateInput!): User
+  }
+
+  type Query {
+    deleteUser(input: UserIdInput!): User
+  }
+
+  input UserIdInput {
+    id: Int
+  }
+
+  input UserCreateInput {
+    id: Int
+    email: String
+    name: String
+    password: String
+    telefono: String
+    id_rol: Int
+  }
+
+  type User {
+    id: Int
+    email: String
+    name: String
+    password: String
+    telefono: String
+    id_rol: Int
+    rol: UserRol
+  }
+`
