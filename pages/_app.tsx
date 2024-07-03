965d13d7328b83518e6804c4e1a07b7636127a19
juanplazas <juan-2151@hotmail.com>
2024-07-03 00:03:50 -0500

    se agrega apollo client

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,15 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import Sidebar from "@/components/ui/Sidebar";
+import { ApolloProvider } from '@apollo/client';
+import client from "@/lib/client"
 
 export default function App({ Component, pageProps }: AppProps) {
   return <div className="flex flex-row items-center">
     <Sidebar />
-    <Component {...pageProps}/>
+    <ApolloProvider client={client}>
+      <Component {...pageProps}/>
+    </ApolloProvider>
   </div> 
   ;
 }
